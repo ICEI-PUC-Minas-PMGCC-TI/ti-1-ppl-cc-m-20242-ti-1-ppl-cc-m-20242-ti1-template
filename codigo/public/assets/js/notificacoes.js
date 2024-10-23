d98b7ce32c244158d0d6a3fa13a428cce2afe482
ilipehwd <ilipedesousa@gmail.com>
2024-10-23 15:40:41 -0300

    intregracao_js_com_json

--- codigo/public/assets/js/notificacoes.js
+++ codigo/public/assets/js/notificacoes.js
@@ -1,3 +1,11 @@
+fetch('../db/db.json')
+  .then(response => response.json())
+  .then(data => {
+    exibirNotificacoes(data); 
+  })
+  .catch(error => console.error('Erro ao carregar o JSON:', error)); 
+
+
 function notif (){
     let nome = document.getElementById("txtNome").value;
     let horario = document.getElementById("txtHorario").value;
@@ -9,14 +17,35 @@ function notif (){
             <p>${ horario } </p>
             <p>${ situacao } </p>
         </div>
-        `
+        `;
 
-    document.getElementById("container").innerHTML += notificacoes
+    document.getElementById("container").innerHTML += notificacoes;
+}
+
+function exibirNotificacoes(data){
+    const notificacoes = data.notificacoes;
+    let notificacoesHTML = '';
+
+    for (let i = 0; i < notificacoes.length; i++){
+        let nome = notificacoes[i].nome;
+        let horario = notificacoes[i].horario;
+        let situacao = notificacoes[i].situacao;
+
+        notificacoesHTML += `
+        <div class="content">
+            <h2>${ nome }</h2>
+            <p>${ horario } </p>
+            <p>${ situacao } </p>
+        </div>
+        `;
+
+        document.getElementById("container").innerHTML = notificacoesHTML;
+    }
 }
 
 function init (){
     let btnNovaNotif = document.getElementById("btnNovaNotif");
-    btnNovaNotif.addEventListener('click', notif);
+    btnNovaNotif.addEventListener('click', exibirNotificacoes);
 }
 
 window.addEventListener('load', init)
