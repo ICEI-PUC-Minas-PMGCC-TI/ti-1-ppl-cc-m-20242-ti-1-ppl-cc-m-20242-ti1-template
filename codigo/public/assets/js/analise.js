b4cc9e20500170cb5dd7846a0e014903dbf11547
Thiago Henrique Gomes Feliciano <1543790@sga.pucminas.br>
2024-10-24 22:19:13 -0300

    Update analise.js

--- codigo/public/assets/js/analise.js
+++ codigo/public/assets/js/analise.js
@@ -21,3 +21,47 @@ var menuSide = document.querySelector('.menu-lateral')
 btnExp.addEventListener('click', function(){
     menuSide.classList.toggle('expandir')
 })
+
+//Gráfico
+
+fetch('data.json')
+    .then(response => response.json())
+    .then(parsedData => {
+        const porcentagensRealizadas = parsedData.habitosRealizados.map(habito => (habito / parsedData.habitosTotais) * 100);
+
+        const ctx = document.getElementById('lineChart').getContext('2d');
+        const lineChart = new Chart(ctx, {
+            type: 'line', 
+            data: {
+                labels: parsedData.dias, 
+                datasets: [{
+                    label: 'Porcentagem de Hábitos Realizados',
+                    data: porcentagensRealizadas,
+                    fill: false,
+                    borderColor: 'rgba(25, 196, 99, 1)', 
+                    tension: 0.1 
+                }]
+            },
+            options: {
+                responsive: true, 
+                maintainAspectRatio: false, 
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                        suggestedMax: 100, 
+                        title: {
+                            display: true,
+                            text: 'Porcentagem (%)'
+                        }
+                    },
+                    x: {
+                        title: {
+                            display: true,
+                            text: 'Dias da Semana'
+                        }
+                    }
+                }
+            }
+        });
+    })
+    .catch(error => console.error('Erro ao carregar o JSON:', error));
