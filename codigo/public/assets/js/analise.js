22ee83fcd279737126dbc5bf542d4276c9c4ff13
Thiago Henrique Gomes Feliciano <1543790@sga.pucminas.br>
2024-10-26 15:36:05 -0300

    Update analise.js

--- codigo/public/assets/js/analise.js
+++ codigo/public/assets/js/analise.js
@@ -65,3 +65,61 @@ fetch('data.json')
         });
     })
     .catch(error => console.error('Erro ao carregar o JSON:', error));
+
+//Gráfico expecifico por hábito
+
+    document.addEventListener("DOMContentLoaded", () => {
+        const customHabitCard = document.getElementById('customHabitCard');
+        const customChartContainer = document.getElementById('customChartContainer');
+    
+        customHabitCard.addEventListener('click', () => {
+            customChartContainer.style.display = customChartContainer.style.display === 'none' || customChartContainer.style.display === '' ? 'block' : 'none';
+            if (customChartContainer.style.display === 'block') {
+                loadCustomChart();
+            }
+        });
+    
+        function loadCustomChart() {
+            fetch('data1.json')
+                .then(response => response.json())
+                .then(parsedData => {
+                    const porcentagensRealizadas = parsedData.habitosRealizados.map(habito => habito * 100);
+    
+                    const ctx = document.getElementById('customLineChart').getContext('2d');
+                    new Chart(ctx, {
+                        type: 'line',
+                        data: {
+                            labels: parsedData.dias,
+                            datasets: [{
+                                label: 'Hábito Realizado (%)',
+                                data: porcentagensRealizadas,
+                                fill: false,
+                                borderColor: 'rgba(25, 196, 99, 1)',
+                                tension: 0.1
+                            }]
+                        },
+                        options: {
+                            responsive: true,
+                            maintainAspectRatio: false,
+                            scales: {
+                                y: {
+                                    beginAtZero: true,
+                                    suggestedMax: 100,
+                                    title: {
+                                        display: true,
+                                        text: 'Porcentagem (%)'
+                                    }
+                                },
+                                x: {
+                                    title: {
+                                        display: true,
+                                        text: 'Dias da Semana'
+                                    }
+                                }
+                            }
+                        }
+                    });
+                })
+                .catch(error => console.error('Erro ao carregar o JSON:', error));
+        }
+    });
