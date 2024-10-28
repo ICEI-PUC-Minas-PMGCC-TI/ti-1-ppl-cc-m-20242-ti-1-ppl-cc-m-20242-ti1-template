390268ffd68236204ec09524b11dac55393d1f72
ilipehwd <ilipedesousa@gmail.com>
2024-10-28 11:18:57 -0300

    arrumando_cods

--- codigo/public/assets/js/notificacoes.js
+++ codigo/public/assets/js/notificacoes.js
@@ -49,4 +49,36 @@ function init (){
     btnNovaNotif.addEventListener('click', exibirNotificacoes);
 }
 
-window.addEventListener('load', init)
+window.addEventListener('load', init);
+
+//Selecionar o item que for clicado
+
+var menuItem = document.querySelectorAll('.item-menu')
+
+function selectLink(){
+    menuItem .forEach((item) =>
+        item.classList.remove('ativo')
+)
+this.classList.add('ativo')
+}
+
+//Espandir o menu
+
+menuItem.forEach((item) =>
+    item.addEventListener('click', selectLink)
+)
+
+var btnExp = document.querySelector('#btn-exp')
+var menuSide = document.querySelector('.menu-lateral')
+
+btnExp.addEventListener('click', function(){
+    menuSide.classList.toggle('expandir')
+})
+
+
+var btnExp = document.querySelector('#btn-exp')
+var menuSide = document.querySelector('.menu-lateral')
+
+btnExp.addEventListener('click', function(){
+    menuSide.classList.toggle('expandir')
+})
