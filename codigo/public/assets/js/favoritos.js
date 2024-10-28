f32a68bb05c79c3cf5b0f1e99606cc6b750828a4
Rafaelhs2005 <rhenriques.2005@gmail.com>
2024-10-27 23:17:15 -0300

    favoritos js

--- codigo/public/assets/js/favoritos.js
+++ codigo/public/assets/js/favoritos.js
@@ -1,3 +1,5 @@
+const usuarioAtualId = 1
+
 async function fetchUserName() {
     try{
         const response = await fetch('/codigo/db/db.json')
@@ -9,5 +11,26 @@ async function fetchUserName() {
         console.error('Erro ao buscar o nome do usuário:', error)
     }
 }
+
+function exibirFavoritos(idUsuario) {
+    const favoritosDoUsuario = dados.favoritos.filter(fav => fav.id_usuario === idUsuario);
+    const eventosFavoritos = dados.eventos.filter(evento => favoritosDoUsuario.some(fav.id_evento === evento.id));
+
+    const container = document.getElementById('eventosFavoritos');
+    container.innerHTML = '';
+    eventosFavoritos.forEach(evento => {
+        const eventoHTML = `
+          <div class="evento">
+            <h2>${evento.nome}</h2>
+            <p>${evento.descricao}</p>
+            <p>Data: ${evento.data}</p>
+          </div>
+          <hr>
+        `;
+        container.innerHTML += eventoHTML;
+      });
+}
+
+exibirFavoritos(usuarioAtualId);
 
 document.addEventListener('DOMContentLoaded', fetchUserName);
