46d6f62f1bcf95138327d869fa344d531c43e8c6
Thiago Henrique Gomes Feliciano <1543790@sga.pucminas.br>
2024-11-15 12:58:06 -0300

    Update analise.js

--- codigo/public/assets/js/analise.js
+++ codigo/public/assets/js/analise.js
@@ -24,7 +24,7 @@ btnExp.addEventListener('click', function(){
 
 //Gráfico
 
-fetch('../db/db.json')
+fetch('data.json')
     .then(response => response.json())
     .then(parsedData => {
         const porcentagensRealizadas = parsedData.habitosRealizados.map(habito => (habito / parsedData.habitosTotais) * 100);
@@ -139,3 +139,46 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 });
+
+
+//Adicionar item a lista, deletar, marcar.
+
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-box");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("Escreva algo primeiro!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
